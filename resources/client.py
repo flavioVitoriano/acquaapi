d514d50c8740397a8c0ed1466782f3d10fa0c8b0
fvitoriano <flavio.vitorianodev@gmail.com>
2020-10-25 16:35:10 -0300

    build client name filter full_name_contains

--- resources/client.py
+++ resources/client.py
@@ -1,8 +1,21 @@
 from models import Client
 from .base import BaseResource, BaseSingleResource
+from flask_restful import reqparse
+
+
+page_parser = reqparse.RequestParser()
+page_parser.add_argument("full_name_contains", type=str, default="")
 
 
 class ClientResource(BaseResource):
+    def filter(self, data):
+        args = page_parser.parse_args()
+        name = args.full_name_contains
+
+        data = data.select().where(Client.full_name.contains(name))
+
+        return data
+
     class Meta:
         model = Client
         replace_fields = []
